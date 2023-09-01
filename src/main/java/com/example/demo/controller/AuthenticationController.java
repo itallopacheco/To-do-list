a990294b57243aa10528f43ed68efc81bf82eea3
itallopacheco <itallopss@dcomp.ufs.br>
2023-08-31 22:07:45 -0300

    :construction: tratamento de exceções

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -4,10 +4,11 @@ import com.example.demo.domain.user.AuthenticationDTO;
 import com.example.demo.domain.user.LoginResponseDTO;
 import com.example.demo.domain.user.RegisterDTO;
 import com.example.demo.domain.user.User;
+import com.example.demo.exceptions.UsernameAlreadyInUseException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.TokenService;
-import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,23 +31,37 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthenticationDTO authDTO){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(authDTO.username(), authDTO.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
+        try{
+            var usernamePassword = new UsernamePasswordAuthenticationToken(authDTO.username(), authDTO.password());
+            var auth = authenticationManager.authenticate(usernamePassword);
 
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+            var token = tokenService.generateToken((User) auth.getPrincipal());
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody RegisterDTO registerDTO){
-        if (userRepository.findByUsername(registerDTO.username()) != null) return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> register(@RequestBody RegisterDTO registerDTO){
+        try{
+            if(userRepository.findByUsername(registerDTO.username()) != null){
+                throw new UsernameAlreadyInUseException(registerDTO.username());
+            }
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
-        User newUser = new User(registerDTO.username(), encryptedPassword, registerDTO.role());
+            String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
+            User newUser = new User(registerDTO.username(), encryptedPassword, registerDTO.role());
+            userRepository.save(newUser);
+            return ResponseEntity.ok().build();
 
-        userRepository.save(newUser);
-        return ResponseEntity.ok().build();
+        }catch (UsernameAlreadyInUseException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
 }
