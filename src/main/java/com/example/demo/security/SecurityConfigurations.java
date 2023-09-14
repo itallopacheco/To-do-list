0b2947b0cccb2f2b9e5d466d9213e50bf5de1295
itallopacheco <itallopss@dcomp.ufs.br>
2023-09-14 18:45:38 -0300

    :construction: abrindo os endpoints temporariamente

--- src/main/java/com/example/demo/security/SecurityConfigurations.java
+++ src/main/java/com/example/demo/security/SecurityConfigurations.java
@@ -30,7 +30,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/ping").authenticated()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
