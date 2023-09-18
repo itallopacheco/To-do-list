31fe28a9f3284785015ea36492fd58f29bc3f0ad
itallopacheco <itallopss@dcomp.ufs.br>
2023-09-18 19:18:28 -0300

    :construction: CRUD de projetos

--- src/main/java/com/example/demo/controller/ProjectController.java
+++ src/main/java/com/example/demo/controller/ProjectController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.domain.project.Project;
 import com.example.demo.domain.project.dto.CreateProjectDTO;
 import com.example.demo.domain.project.dto.ProjectDTO;
+import com.example.demo.domain.project.dto.UpdateProjectDTO;
 import com.example.demo.domain.user.User;
 import com.example.demo.repository.ProjectRepository;
 import com.example.demo.services.ProjectService;
@@ -21,15 +22,31 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
-    @PostMapping("/add")
+    @PostMapping()
     public ResponseEntity<ProjectDTO> createProject(@RequestBody CreateProjectDTO createProjectDTO, @AuthenticationPrincipal User authenticatedUser){
         createProjectDTO.setOwnerId(authenticatedUser.getId());
         return new ResponseEntity<ProjectDTO>(projectService.createProject(createProjectDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping("/all")
+    @GetMapping()
     public ResponseEntity<List<ProjectDTO>> getAll(){
         return new ResponseEntity<List<ProjectDTO>>(projectService.getAllProjects(),HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public  ResponseEntity<ProjectDTO> getById(@PathVariable(name = "id") long id){
+        return new ResponseEntity<ProjectDTO>(projectService.getProjectById(id),HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ProjectDTO> update(@PathVariable(name = "id") long id, @RequestBody UpdateProjectDTO updateProjectDTO){
+        return new ResponseEntity<ProjectDTO>(projectService.updateProject(id,updateProjectDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id")long id){
+
+        return new ResponseEntity<>((projectService.deleteProject(id)),HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/{id}/add-member/{idUser}")
@@ -37,6 +54,12 @@ public class ProjectController {
         projectService.addMember(projetoId,userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
 
+    }
+
+    @PostMapping("/{id}/remove-member/{idUser}")
+    public ResponseEntity<?> deleteMember(@PathVariable(name = "id")long projetoId, @PathVariable(name = "idUser") Long userId){
+        projectService.removeMember(projetoId,userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
