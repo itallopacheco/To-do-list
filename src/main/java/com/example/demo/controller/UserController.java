c51d481c7d7388bc332e3d517ca63283c9b8f3e3
itallo <istmujin6@gmail.com>
2023-09-19 15:38:26 -0300

    :recycle: mudando nome dos metodos

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.domain.user.dto.UserDTO;
 import com.example.demo.domain.user.dto.UserUpdateDTO;
 import com.example.demo.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,18 +25,18 @@ public class UserController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserDTO> findUserById(@PathVariable(value = "id") long id){
+    public ResponseEntity<UserDTO> findById(@PathVariable(value = "id") long id){
        UserDTO userDTO = userService.findById(id);
        return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
     }
 
     @PutMapping({"{id}"})
-    public ResponseEntity<UserDTO> updateUser(@PathVariable(value = "id")long id ,@RequestBody UserUpdateDTO userUpdateDTO){
+    public ResponseEntity<UserDTO> update(@PathVariable(value = "id")long id ,@RequestBody @Valid UserUpdateDTO userUpdateDTO){
         return ResponseEntity.ok(userService.updateUser(id,userUpdateDTO));
     }
 
     @DeleteMapping({"{id}"})
-    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") long id){
+    public ResponseEntity<?> delete(@PathVariable(value = "id") long id){
         userService.deleteUser(id);
         return ResponseEntity.ok().build();
     }
