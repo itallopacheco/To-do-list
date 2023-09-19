a80024b9164a1363a15424c7814868ec6b54c46c
itallopacheco <itallopss@dcomp.ufs.br>
2023-09-19 09:39:30 -0300

    :sparkle: tratamento de exceção

---
+++ src/main/java/com/example/demo/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.demo.services.exceptions;
+
+public class EntityNotFoundException  extends  RuntimeException{
+
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+
+
+
+}
