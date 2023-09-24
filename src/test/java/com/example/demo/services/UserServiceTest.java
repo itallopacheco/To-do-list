1fea16e137f4fc48eb1b2a24e4ccd5c32a3f8ccf
itallopacheco <itallopss@dcomp.ufs.br>
2023-09-24 11:55:03 -0300

    :construction: testando save

--- src/test/java/com/example/demo/services/UserServiceTest.java
+++ src/test/java/com/example/demo/services/UserServiceTest.java
@@ -3,9 +3,11 @@ package com.example.demo.services;
 import com.example.demo.domain.enums.UserRole;
 import com.example.demo.domain.project.dto.ProjectDTO;
 import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.dto.UserCreationDTO;
 import com.example.demo.domain.user.dto.UserDTO;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.exceptions.EntityNotFoundException;
+import com.example.demo.services.exceptions.UniqueFieldAlreadyExists;
 import com.example.demo.services.imp.UserServiceImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,7 +25,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -44,6 +46,7 @@ class UserServiceTest {
     private UserRepository userRepository;
 
     private User user;
+    private UserCreationDTO userCreationDTO;
     private UserDTO userDTO;
 
     private Optional<User> optionalUser;
@@ -57,7 +60,55 @@ class UserServiceTest {
     }
 
     @Test
-    void save() {
+    void whenSaveThenReturnSuccess() {
+        when(userRepository.save(any())).thenReturn(user);
+
+        UserDTO response = userService.save(userCreationDTO);
+
+        assertNotNull(response);
+        assertEquals(UserDTO.class,response.getClass());
+        assertEquals(NAME,response.getName());
+        assertEquals(BIRTHDATE,response.getBirthDate());
+        assertEquals(EMAIL,response.getEmail());
+        assertEquals(USERNAME,response.getUsername());
+
+    }
+    @Test
+    void whenSaveThenReturnEmailAlreadyExistException() {
+        when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+        try{
+            userService.save(userCreationDTO);
+        } catch (Exception ex){
+            assertEquals(UniqueFieldAlreadyExists.class, ex.getClass());
+            assertEquals("Email: " +EMAIL+ " already in use",ex.getMessage());
+        }
+
+    }
+    @Test
+    void whenSaveThenReturnUsernameAlreadyExistException() {
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        try{
+            userService.save(userCreationDTO);
+        } catch (Exception ex){
+            assertEquals(UniqueFieldAlreadyExists.class, ex.getClass());
+            assertEquals("Username: " +USERNAME+ " already in use",ex.getMessage());
+        }
+
+    }
+    @Test
+    void whenSaveThenReturnEmailAndUsernameAlreadyExistException() {
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+        when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+        try{
+            userService.save(userCreationDTO);
+        } catch (Exception ex){
+            assertEquals(UniqueFieldAlreadyExists.class, ex.getClass());
+            assertEquals("Email: " + EMAIL + " and Username: " + USERNAME + " already in use",ex.getMessage());
+        }
+
     }
 
     @Test
@@ -120,6 +171,8 @@ class UserServiceTest {
         userDTO = new UserDTO(user);
         pageUsers = new PageImpl<>(userDTOList, PageRequest.of(0, 10), userDTOList.size());
         optionalUser = Optional.of(user);
+        userCreationDTO = new UserCreationDTO(NAME,BIRTHDATE,EMAIL,USERNAME,PASSWORD,PASSWORD);
+
     }
 
 
